54d9e3dfd1d8ea82462ffea0e6cbe38125e2a70c
yasinchan <yasinchan2016@gmail.com>
2018-09-29 10:00:25 +0800

    update

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -8,7 +8,7 @@
       </nuxt-link>
       <div class="rightTitle">
         <nuxt-link to="/post/1">
-          <h3>第一个标题</h3>
+          <h3>Website is under construction</h3>
         </nuxt-link>
         <nuxt-link to="/tag/1" class="rightTag">标签一</nuxt-link>
         <nuxt-link to="/tag/4" class="rightTag">标签四</nuxt-link>
@@ -22,7 +22,7 @@
       </nuxt-link>
       <div class="rightTitle">
         <nuxt-link to="/post/2">
-          <h3>第一个标题</h3>
+          <h3>Website is under construction</h3>
         </nuxt-link>
         <nuxt-link to="/tag/2" class="rightTag">标签二</nuxt-link>
       </div>
@@ -35,7 +35,7 @@
       </nuxt-link>
       <div class="rightTitle">
         <nuxt-link to="/post/3">
-          <h3>第一个标题</h3>
+          <h3>Website is under construction</h3>
         </nuxt-link>
         <nuxt-link to="/tag/3" class="rightTag">标签三</nuxt-link>
       </div>
